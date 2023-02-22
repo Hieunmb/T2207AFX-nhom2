18bd3a44a0ac6753ca63f604a6a1af9ec5f6a8db
ChuDucHoangg <110522309+ChuDucHoangg@users.noreply.github.com>
2023-02-22 22:30:17 +0700

    commit

--- src/controller/bills/BillsController.java
+++ src/controller/bills/BillsController.java
@@ -13,4 +13,32 @@ public class BillsController {
         HomeController.rootStage.setScene(new Scene(root, 1200, 720));
         HomeController.rootStage.setTitle("Home");
     }
+
+    public void goToCheckin(ActionEvent actionEvent) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../../resources/checkin/checkin.fxml"));
+
+        HomeController.rootStage.setScene(new Scene(root, 1200, 720));
+        HomeController.rootStage.setTitle("Check In");
+    }
+
+    public void goToService(ActionEvent actionEvent) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../../resources/service/service.fxml"));
+
+        HomeController.rootStage.setScene(new Scene(root, 1200, 720));
+        HomeController.rootStage.setTitle("Service");
+    }
+
+    public void goToManageRoom(ActionEvent actionEvent) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../../resources/room/room.fxml"));
+
+        HomeController.rootStage.setScene(new Scene(root, 1200, 720));
+        HomeController.rootStage.setTitle("Manage Room");
+    }
+
+    public void goToCustomer(ActionEvent actionEvent) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../../resources/customer_info/customer_info.fxml"));
+
+        HomeController.rootStage.setScene(new Scene(root, 1200, 720));
+        HomeController.rootStage.setTitle("Customer Info");
+    }
 }
