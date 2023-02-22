c5db2944576834e11d5a3ec5cbb192d43c5f6bee
Hieunmb <zhuongdae@gmail.com>
2023-02-22 15:09:54 +0700

    hieu

--- src/controller/service/ServiceController.java
+++ src/controller/service/ServiceController.java
@@ -1,4 +1,24 @@
 package controller.service;
 
+import controller.HomeController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
 public class ServiceController {
+
+    public void goToHome(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../../resources/home1.fxml"));
+        HomeController.rootStage.setScene(new Scene(root,1200,720));
+    }
+
+    public void goToCheckIn(ActionEvent event) {
+    }
+
+    public void goToRoom(ActionEvent event) {
+    }
+
+    public void goToBill(ActionEvent event) {
+    }
 }
