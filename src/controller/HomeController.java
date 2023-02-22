e38619837439e16a26ff37ff2b89e9bb4a349c5a
trung-2004 <110414167+trung-2004@users.noreply.github.com>
2023-02-22 14:18:39 +0700

    first commit

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -1,7 +1,37 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class HomeController {
     public static Stage rootStage;
+
+    public void goToManageRoom(ActionEvent event) {
+
+    }
+
+    public void goToCheckIn(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../resources/checkin/checkin.fxml"));
+
+        rootStage.setScene(new Scene(root, 1200, 720));
+        rootStage.setTitle("Check In");
+    }
+
+    public void goToCustomerInfo(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../resources/customer_info/customer_info.fxml"));
+
+        rootStage.setScene(new Scene(root, 1200, 720));
+        rootStage.setTitle("Customer Info");
+    }
+
+    public void goToBill(ActionEvent event) {
+
+    }
+
+    public void goToService(ActionEvent event) {
+
+    }
 }
--- src/controller/checkin/CheckInController.java
+++ src/controller/checkin/CheckInController.java
@@ -1,5 +1,17 @@
 package controller.checkin;
 
+import controller.HomeController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
 public class CheckInController {
 
+    public void goToHome(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../../resources/home1.fxml"));
+
+        HomeController.rootStage.setScene(new Scene(root, 1200, 720));
+        HomeController.rootStage.setTitle("Home");
+    }
 }
